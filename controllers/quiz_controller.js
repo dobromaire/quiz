82e1f3d314fff5245204d2c12d4e4786178a23f3
Antonio Doblado <antonio.doblador@gmail.com>
2015-07-30 23:48:08 +0200

    14 - Borrado  de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -160,3 +160,9 @@ exports.update = function(req, res) {
 	} // redireccion http a lista de preguntas (URL relativo)
 
 }
+
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then(function() {
+		res.redirect("/quizes");
+	}).catch(function(error) {next(error)});
+};
--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,8 @@ router.get("/quizes/:quizId(\\d+)/edit", quizController.edit);
 
 router.put("/quizes/:quizId(\\d+)", quizController.update);
 
+router.get("/quizes/:quizId(\\d+)/edit", quizController.edit);
+
+router.delete("/quizes/:quizId(\\d+)", quizController.destroy);
+
 module.exports = router;
